4d214ff5b9408f05be1d889ca44ae64a958abf82
Jasnaik <Tabish.Jasnaik@travelex.com>
2019-06-29 13:48:33 +0530

    initail commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+const port = process.env.PORT || 9002;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index1.html")
+})
+
+io.on("connection", (client) => {
+    console.log("Connected");
+    client.emit("acknowledge", { message: "Connection established" })
+
+    client.on("MsgToServer", (chatterName,message) => {
+        console.log(chatterName+" says : " + message);
+        client.emit("MsgToClient",'Me', message);
+    client.broadcast.emit("MsgToClient",chatterName,message);
+})
+})
+
+server.listen(port, () => {
+    console.log("Socket server started at :" + port);
+})
+
+
+
+
